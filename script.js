57e75d9e4fe815c025a47266d33848989fcbf6dc
julieli1 <>
2021-04-08 15:12:02 +0000

    functions

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let startmin = 0
 // converts minutes into seconds
 let time = startmin * 60
 
-document.getElementById('start').addEventListener('click', countdown)
+document.getElementById('start').addEventListener('click', interval)
 
-function countdown () {
+function interval () {
   // updates the startmin into input value
   startmin = document.getElementById('input').value
   startmin = parseInt(startmin)
@@ -13,23 +13,27 @@ function countdown () {
 
   // updates the countown every one second
   let x = setInterval(countdown, 1000)
+  
+}
+
+function countdown () {
   // calculates how many minutes are left
-  const minutes = Math.floor(time / 60)
+  let minutes = Math.floor(time / 60)
   // modulus, division remainder
-  const seconds = time % 60
+  let seconds = time % 60
+
+  // updates the time negatively so that it's a countdown and not a timer
+  time--
 
   // makes it two digits so it's more aesthetically pleasing
   const m = String(minutes).padStart(2, '0')
   const s = String(seconds).padStart(2, '0')
 
-  // updates the time negatively so that it's a countdown and not a timer
-  time--
-
   // updates the html so we can see the timer going
   document.getElementById('time-left').innerHTML = m + ':' + s
 
   // prevents the timer going into negative time
-  while ((minutes <= 0) && (seconds <= 0)) {
+  if ((minutes <= 0) && (seconds <= 0)) {
     x = clearInterval(x)
   }
 }
